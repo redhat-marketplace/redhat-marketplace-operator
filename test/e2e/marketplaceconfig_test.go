387284abc48488dc9287d3217e872665dc936590
Namsimar Singh <namsimar@ibm.com>
2020-03-17 09:06:32 -0400

    chore: add e2e tests

--- test/e2e/marketplaceconfig_test.go
+++ test/e2e/marketplaceconfig_test.go
@@ -3,11 +3,15 @@
 package e2e
 
 import (
+	goctx "context"
+	"fmt"
 	"testing"
 	"time"
 
+	"github.com/spf13/viper"
 	"github.ibm.com/symposium/marketplace-operator/pkg/apis"
-	operator "github.ibm.com/symposium/marketplace-operator/pkg/apis/marketplace/v1alpha1"
+	marketplacev1alpha1 "github.ibm.com/symposium/marketplace-operator/pkg/apis/marketplace/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
 	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
@@ -21,15 +25,16 @@ var (
 )
 
 func TestMarketplaceConfig(t *testing.T) {
-	marketplaceConfigList := &operator.MarketplaceConfigList{}
+	marketplaceConfigList := &marketplacev1alpha1.MarketplaceConfigList{}
 	err := framework.AddToFrameworkScheme(apis.AddToScheme, marketplaceConfigList)
 	if err != nil {
 		t.Fatalf("failed to add custom resource scheme to framework: %v", err)
 	}
+	viper.Set("autoinstall", true)
 	// run subtests
 	t.Run("marketplaceconfig-group", func(t *testing.T) {
 		t.Run("Cluster3", MarketplaceOperatorCluster)
-		t.Run("Cluster4", MarketplaceOperatorCluster)
+		// t.Run("Cluster4", MarketplaceOperatorCluster)
 	})
 }
 
@@ -54,4 +59,42 @@ func MarketplaceOperatorCluster(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	exampleMarketplaceConfig := &marketplacev1alpha1.MarketplaceConfig{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "example-marketplaceconfig",
+			Namespace: namespace,
+		},
+		Spec: marketplacev1alpha1.MarketplaceConfigSpec{
+			Size: 1,
+		},
+	}
+
+	err = f.Client.Create(goctx.TODO(), exampleMarketplaceConfig, &framework.CleanupOptions{TestContext: ctx, Timeout: timeout, RetryInterval: retryInterval})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err = crDeployedTest(t, f, ctx, "example-marketplaceconfig", 1); err != nil {
+		t.Error(err)
+	}
+	if err = crDeployedTest(t, f, ctx, "marketplaceconfig-razeedeployment", 1); err != nil {
+		t.Error(err)
+	}
+	if err = crDeployedTest(t, f, ctx, "marketplaceconfig-meterbase", 1); err != nil {
+		t.Error(err)
+	}
+}
+
+// Test that when MarketplaceConfig CR is deployed, so is a RazeePod
+func crDeployedTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx, name string, replicas int) error {
+	namespace, err := ctx.GetNamespace()
+
+	err = e2eutil.WaitForDeployment(t, f.KubeClient, namespace, name, replicas, time.Second*5, time.Second*30)
+	if err != nil {
+		return fmt.Errorf("Failed waiting for deployment %v", err)
+	}
+
+	return nil
+
 }
