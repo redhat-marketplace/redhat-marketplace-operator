8f227b171ca89731ca080b89f3e6d2d65a2160bd
maxpaspa <maxpaspa@ibm.com>
2020-04-06 12:48:41 -0400

    removed unused import

--- pkg/controller/marketplaceconfig/marketplaceconfig_controller.go
+++ pkg/controller/marketplaceconfig/marketplaceconfig_controller.go
@@ -10,7 +10,6 @@ import (
 	"github.ibm.com/symposium/marketplace-operator/pkg/utils"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
