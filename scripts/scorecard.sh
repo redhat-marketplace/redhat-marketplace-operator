ee33586e7acf48d5f9de3510193770bec0bbdf2f
Zach Taylor <ztaylor@ibm.com>
2020-06-09 00:22:07 -0400

    chore: fixing scorecard

--- scripts/scorecard.sh
+++ scripts/scorecard.sh
@@ -7,10 +7,13 @@ kubectl create ns openshift-redhat-marketplace
 kubectl config set-context --current --namespace=openshift-redhat-marketplace
 kubectl apply -f deploy/role.yaml -f deploy/service_account.yaml -f deploy/role_binding.yaml
 
+sleep 10
+
 echo "Running scorecard"
 operator-sdk scorecard -b ./deploy/olm-catalog/redhat-marketplace-operator --config ./.osdk-scorecard.yaml -o json >scorecard-output.json
 
 echo "Scorecard complete"
+
 
 cat scorecard-output.json
 
