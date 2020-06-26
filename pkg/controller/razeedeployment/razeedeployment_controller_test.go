c742d875f0f7297958a53368d57c34bda81b8dbe
maxpaspa <maxpaspa@ibm.com>
2020-06-26 12:42:16 -0400

    legacy uninstall test

--- pkg/controller/razeedeployment/razeedeployment_controller_test.go
+++ pkg/controller/razeedeployment/razeedeployment_controller_test.go
@@ -51,6 +51,7 @@ func TestRazeeDeployController(t *testing.T) {
 	t.Run("Test No Secret", testNoSecret)
 	t.Run("Test Bad Name", testBadName)
 	t.Run("Test Full Uninstall", testFullUninstall)
+	t.Run("Test Legacy Uninstall",testLegacyUninstall)
 }
 
 func newUnstructured(apiVersion, kind, namespace, name string) *unstructured.Unstructured {
@@ -277,6 +278,61 @@ func testFullUninstall(t *testing.T) {
 				list, ok := i.(*corev1.SecretList)
 
 				assert.Truef(t, ok, "expected secret list got type %T", i)
+				assert.Equal(t, 0, len(list.Items))
+			})),
+		ListStep(opts,
+			ListWithObj(&batch.JobList{}),
+			ListWithFilter(
+				client.InNamespace(namespace),
+			),
+			ListWithCheckResult(func(r *ReconcilerTest, t *testing.T, i runtime.Object) {
+				list, ok := i.(*batch.JobList)
+
+				assert.Truef(t, ok, "expected job list got type %T", i)
+				assert.Equal(t, 0, len(list.Items))
+			})),
+		ListStep(opts,
+			ListWithObj(&appsv1.DeploymentList{}),
+			ListWithFilter(
+				client.InNamespace(namespace),
+			),
+			ListWithCheckResult(func(r *ReconcilerTest, t *testing.T, i runtime.Object) {
+				list, ok := i.(*appsv1.DeploymentList)
+
+				assert.Truef(t, ok, "expected deployment list got type %T", i)
+				assert.Equal(t, 0, len(list.Items))
+			})),
+	)
+}
+
+func testLegacyUninstall(t *testing.T) {
+	t.Parallel()
+
+	reconcilerTest := NewReconcilerTest(setup,
+		&razeeDeploymentDeletion,
+		&razeeJob,
+		&razeeSecret,
+		&configMap,
+		&serviceAccount,
+		&deployment,
+	)
+
+	reconcilerTest.TestAll(t,
+		//Requeue until we have created the job and waiting for it to finish
+		ReconcileStep(opts,
+			ReconcileWithExpectedResults(
+				append(
+					RangeReconcileResults(RequeueResult, 2),
+					AnyResult)...)),
+		ListStep(opts,
+			ListWithObj(&corev1.ServiceAccountList{}),
+			ListWithFilter(
+				client.InNamespace(namespace),
+			),
+			ListWithCheckResult(func(r *ReconcilerTest, t *testing.T, i runtime.Object) {
+				list, ok := i.(*corev1.ServiceAccountList)
+
+				assert.Truef(t, ok, "expected service account list got type %T", i)
 				assert.Equal(t, 0, len(list.Items))
 			})),
 		ListStep(opts,
