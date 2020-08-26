21da8ee29f30a50b9abfb924bf1e83763623ea5d
Namsimar Singh <namsimar@ibm.com>
2020-08-26 09:19:25 -0400

    fix: updated MarketplaceConfig name to follow convention

--- pkg/utils/env.go
+++ pkg/utils/env.go
@@ -28,7 +28,7 @@ const (
 	COS_READER_KEY_NAME                    = "rhm-cos-reader-key"
 	RAZEE_UNINSTALL_NAME                   = "razee-uninstall-job"
 	RHM_OPERATOR_SECRET_NAME               = "rhm-operator-secret"
-	MARKETPLACECONFIG_NAME                 = "marketplaceconfig"
+	MARKETPLACECONFIG_NAME                 = "rhm-marketplaceconfig"
 	METERBASE_NAME                         = "rhm-marketplaceconfig-meterbase"
 	RAZEE_NAME                             = "rhm-marketplaceconfig-razeedeployment"
 	OPSRC_NAME                             = "redhat-marketplace"
@@ -71,9 +71,8 @@ const (
 	CSV_ANNOTATION_NAMESPACE       = "csvNamespace"
 	CSV_METERDEFINITION_ANNOTATION = "marketplace.redhat.com/meterDefinition"
 
-
 	/* Time and Date */
-	DATE_FORMAT = "2006-01-02"
+	DATE_FORMAT         = "2006-01-02"
 	METER_REPORT_PREFIX = "meter-report-"
 )
 
