23cdb17f316979d0a20321b7f6d32bbf3363d371
Rajat-Godi <Rajat.Godi@ibm.com>
2021-03-24 13:03:54 +0530

    Added implementation for UploadFile, log changes

--- airgap/v2/pkg/server/server.go
+++ airgap/v2/pkg/server/server.go
@@ -16,59 +16,92 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"io"
 
+	"github.com/go-logr/logr"
 	"github.com/redhat-marketplace/redhat-marketplace-operator/airgap/v2/apis/fileserver"
+	v1 "github.com/redhat-marketplace/redhat-marketplace-operator/airgap/v2/apis/model/v1"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 type Server struct {
 	fileserver.UnimplementedFileServerServer
+	Log logr.Logger
 }
 
-func (*Server) UploadFile(stream fileserver.FileServer_UploadFileServer) error {
+func (s *Server) UploadFile(stream fileserver.FileServer_UploadFileServer) error {
+	var bs []byte
+	var fid *v1.FileID
+	var size uint32
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			s.Log.Info(fmt.Sprintf("Stream end, total bytes received: %v", len(bs)))
+			res := &fileserver.UploadFileResponse{
+				FileId: fid,
+				Size:   size,
+			}
+			//TODO: Add database save here
+			return stream.SendAndClose(res)
+		} else if err != nil {
+			s.Log.Error(err, "Oops, something went wrong!")
+			return status.Errorf(
+				codes.Unknown,
+				fmt.Sprintf("Error while processing stream, details: %v", err),
+			)
+		}
+
+		b := req.GetChunkData()
+		if b != nil && bs == nil {
+			bs = b
+		} else if b != nil {
+			bs = append(bs, b...)
+		} else if req.GetInfo() != nil {
+			fid = req.GetInfo().GetFileId()
+			size = req.GetInfo().GetSize()
+		}
+	}
+}
+
+func (s *Server) ListFileMetadata(lis *fileserver.ListFileMetadataRequest, stream fileserver.FileServer_ListFileMetadataServer) error {
 	return status.Errorf(
 		codes.Unimplemented,
 		"Method Unimplemented",
 	)
 }
 
-func (*Server) ListFileMetadata(lis *fileserver.ListFileMetadataRequest, stream fileserver.FileServer_ListFileMetadataServer) error {
+func (s *Server) GetFileMetadata(ctx context.Context, gfmr *fileserver.GetFileMetadataRequest) (*fileserver.GetFileMetadataResponse, error) {
+	return nil, status.Errorf(
+		codes.Unimplemented,
+		"Method Unimplemented",
+	)
+}
+
+func (s *Server) DownloadFile(dfr *fileserver.DownloadFileRequest, stream fileserver.FileServer_DownloadFileServer) error {
 	return status.Errorf(
 		codes.Unimplemented,
 		"Method Unimplemented",
 	)
 }
 
-func (*Server) GetFileMetadata(ctx context.Context, gfmr *fileserver.GetFileMetadataRequest) (*fileserver.GetFileMetadataResponse, error) {
+func (s *Server) UpdateFileMetadata(ctx context.Context, ufmr *fileserver.UpdateFileMetadataRequest) (*fileserver.UpdateFileMetadataResponse, error) {
 	return nil, status.Errorf(
 		codes.Unimplemented,
 		"Method Unimplemented",
 	)
 }
 
-func (*Server) DownloadFile(dfr *fileserver.DownloadFileRequest, stream fileserver.FileServer_DownloadFileServer) error {
-	return status.Errorf(
-		codes.Unimplemented,
-		"Method Unimplemented",
-	)
-}
-
-func (*Server) UpdateFileMetadata(ctx context.Context, ufmr *fileserver.UpdateFileMetadataRequest) (*fileserver.UpdateFileMetadataResponse, error) {
+func (s *Server) DeleteFile(ctx context.Context, dfr *fileserver.DeleteFileRequest) (*fileserver.DeleteFileResponse, error) {
 	return nil, status.Errorf(
 		codes.Unimplemented,
 		"Method Unimplemented",
 	)
 }
 
-func (*Server) DeleteFile(ctx context.Context, dfr *fileserver.DeleteFileRequest) (*fileserver.DeleteFileResponse, error) {
-	return nil, status.Errorf(
-		codes.Unimplemented,
-		"Method Unimplemented",
-	)
-}
-
-func (*Server) CleanTombstones(ctx context.Context, ctr *fileserver.CleanTombstonesRequest) (*fileserver.CleanTombstonesResponse, error) {
+func (s *Server) CleanTombstones(ctx context.Context, ctr *fileserver.CleanTombstonesRequest) (*fileserver.CleanTombstonesResponse, error) {
 	return nil, status.Errorf(
 		codes.Unimplemented,
 		"Method Unimplemented",
