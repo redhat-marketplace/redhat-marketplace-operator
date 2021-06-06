154b83e363605d26ab7bbe3143e2473697ccd2f4
maxpaspa <maxpaspa@ibm.com>
2021-06-06 16:15:50 -0400

    wire gen

--- reporter/v2/pkg/reporter/wire_gen.go
+++ reporter/v2/pkg/reporter/wire_gen.go
@@ -31,9 +31,7 @@ func NewTask(ctx context.Context, reportName ReportName, config2 *Config) (*Task
 	}
 	logrLogger := _wireLoggerValue
 	clientCommandRunner := reconcileutils.NewClientCommand(simpleClient, scheme, logrLogger)
-	uploaderTarget := config2.UploaderTarget
-	string2 := config2.DeployedNamespace
-	uploader, err := ProvideUploader(ctx, clientCommandRunner, logrLogger, uploaderTarget, string2)
+	uploader, err := ProvideUploader(ctx, clientCommandRunner, logrLogger, config2)
 	if err != nil {
 		return nil, err
 	}
