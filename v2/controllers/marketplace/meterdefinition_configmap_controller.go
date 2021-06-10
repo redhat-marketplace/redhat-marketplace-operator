e2b5e455f5d99193d3f6d6eadb3c28bbcb760c62
MJSiddu <siddumj21@gmail.com>
2021-06-10 18:38:54 -0400

    remove create from rbac

--- v2/controllers/marketplace/meterdefinition_configmap_controller.go
+++ v2/controllers/marketplace/meterdefinition_configmap_controller.go
@@ -183,7 +183,7 @@ func (r *MeterdefConfigMapReconciler) SetupWithManager(mgr ctrl.Manager) error {
 
 }
 
-// +kubebuilder:rbac:groups=apps.openshift.io,resources=deploymentconfigs,verbs=get;list;watch;create
+// +kubebuilder:rbac:groups=apps.openshift.io,resources=deploymentconfigs,verbs=get;list;watch
 
 // Reconcile reads that state of the cluster for a MeterdefConfigmap object and makes changes based on the state read
 // and what is in the MeterdefConfigmap.Spec
