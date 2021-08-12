5f5d34bf5a7198736abf44a13eb5b3d83077dca9
maxpaspa <maxpaspa@ibm.com>
2021-08-12 10:25:56 -0400

    further sync() updates

--- v2/controllers/marketplace/deploymentconfig_controller.go
+++ v2/controllers/marketplace/deploymentconfig_controller.go
@@ -257,7 +257,7 @@ func (r *DeploymentConfigReconciler) sync(request reconcile.Request, reqLogger l
 			return result
 		}
 
-		installedMeterdefs,result := listAllCommunityMeterdefs(r.Client,labelsMap)
+		installedMeterdefs,result := listAllCommunityMeterdefsOnCluster(r.Client,labelsMap)
 		if !result.Is(Continue) {
 			return result
 		}
@@ -265,48 +265,19 @@ func (r *DeploymentConfigReconciler) sync(request reconcile.Request, reqLogger l
 		// get the list of meter defs to be deleted and delete them
 		deleteOnDiff(&meterDefsFromFileServer,&installedMeterdefs.Items)
 
-		// deleteList := []marketplacev1beta1.MeterDefinition{}
-		// foundList := []marketplacev1beta1.MeterDefinition{}
-		for _,mdef := range meterDefsFromFileServer {
-			foundMdef := &marketplacev1beta1.MeterDefinition{}
 
-			err = r.Client.Get(context.TODO(),types.NamespacedName{mdef.Name,mdef.Namespace},foundMdef)
+		for _,catalogMeterdef := range meterDefsFromFileServer {
+			installedMdef := &marketplacev1beta1.MeterDefinition{}
+
+			err = r.Client.Get(context.TODO(),types.NamespacedName{catalogMeterdef.Name,catalogMeterdef.Namespace},installedMdef)
 			/* 
 				create the meterdef if it's in the file server and not on the cluster
 			*/
 			if err != nil && errors.IsNotFound(err){
-					gvk, err := apiutil.GVKForObject(&csv, r.Scheme)
-					if err != nil {
-						return &ExecResult{
-							ReconcileResult: reconcile.Result{},
-							Err:             err,
-						}
+					result = r.createMeterdef(catalogMeterdef,&csv,reqLogger)
+					if !result.Is(Continue) {
+						return result
 					}
-
-					// create owner reference instance
-					ref := metav1.OwnerReference{
-						APIVersion:         gvk.GroupVersion().String(),
-						Kind:               gvk.Kind,
-						Name:               csv.GetName(),
-						UID:                csv.GetUID(),
-						BlockOwnerDeletion: pointer.BoolPtr(false),
-						Controller:         pointer.BoolPtr(false),
-					}
-
-		
-					mdef.ObjectMeta.SetOwnerReferences([]metav1.OwnerReference{ref})
-					mdef.ObjectMeta.Namespace = namespace
-
-					err = r.Client.Create(context.TODO(), &mdef)
-					if err != nil {
-						reqLogger.Error(err, "Failed creating meter definition", "Name", mdef.Name, "Namespace", namespace)
-						return &ExecResult{
-							ReconcileResult: reconcile.Result{},
-							Err:             err,
-						}
-					}
-
-					reqLogger.Info("Created meterdefintion", "Name", mdef.Name, "Namespace", namespace)
 	
 			} else if err != nil {
 					return &ExecResult{
@@ -317,22 +288,9 @@ func (r *DeploymentConfigReconciler) sync(request reconcile.Request, reqLogger l
 				the meterdef is on the cluster see if it needs to be updated
 			*/
 			} else {
-				//TODO: check what values we want to compare here
-				updatedMeterdefinition := foundMdef.DeepCopy()
-				updatedMeterdefinition.Spec = mdef.Spec
-				updatedMeterdefinition.ObjectMeta.Annotations = mdef.ObjectMeta.Annotations
-
-				if !reflect.DeepEqual(updatedMeterdefinition, foundMdef) {
-					reqLogger.Info("meterdefintion is out of sync with latest meterdef catalog", "Name", foundMdef.Name)
-					err = r.Client.Update(context.TODO(), updatedMeterdefinition)
-					if err != nil {
-						reqLogger.Error(err, "Failed updating meter definition", "Name", mdef.Name, "Namespace", namespace)
-						return &ExecResult{
-							ReconcileResult: reconcile.Result{},
-							Err:             err,
-						}
-					}
-					reqLogger.Info("Updated meterdefintion", "Name", mdef.Name, "Namespace", namespace)
+				result := r.updateMeterdef(*installedMdef,catalogMeterdef,reqLogger)
+				if !result.Is(Continue) {
+					return result
 				}
 			}
 		}
@@ -343,6 +301,74 @@ func (r *DeploymentConfigReconciler) sync(request reconcile.Request, reqLogger l
 	}
 }
 
+func (r *DeploymentConfigReconciler) updateMeterdef(installedMdef marketplacev1beta1.MeterDefinition,catalogMdef marketplacev1beta1.MeterDefinition,reqLogger logr.Logger)*ExecResult{
+	updatedMeterdefinition := installedMdef.DeepCopy()
+	updatedMeterdefinition.Spec = catalogMdef.Spec
+	updatedMeterdefinition.ObjectMeta.Annotations = catalogMdef.ObjectMeta.Annotations
+
+	if !reflect.DeepEqual(updatedMeterdefinition, installedMdef) {
+		reqLogger.Info("meterdefintion is out of sync with latest meterdef catalog", "Name", installedMdef.Name)
+		err := r.Client.Update(context.TODO(), updatedMeterdefinition)
+		if err != nil {
+			reqLogger.Error(err, "Failed updating meter definition", "Name", updatedMeterdefinition.Name, "Namespace", updatedMeterdefinition.Namespace)
+			return &ExecResult{
+				ReconcileResult: reconcile.Result{},
+				Err:             err,
+			}
+		}
+		reqLogger.Info("Updated meterdefintion", "Name", updatedMeterdefinition.Name, "Namespace", updatedMeterdefinition.Namespace)
+
+		return &ExecResult{
+			ReconcileResult: reconcile.Result{Requeue: true},
+			Err:             nil,
+		}
+	}
+
+	return &ExecResult{
+		Status: ActionResultStatus(Continue),
+	}
+}
+
+func (r *DeploymentConfigReconciler) createMeterdef(meterDefinition marketplacev1beta1.MeterDefinition, csv *olmv1alpha1.ClusterServiceVersion, reqLogger logr.InfoLogger) *ExecResult {
+	gvk, err := apiutil.GVKForObject(csv, r.Scheme)
+	if err != nil {
+		return &ExecResult{
+			ReconcileResult: reconcile.Result{},
+			Err:             err,
+		}
+	}
+
+	// create owner ref object
+	ref := metav1.OwnerReference{
+		APIVersion:         gvk.GroupVersion().String(),
+		Kind:               gvk.Kind,
+		Name:               csv.GetName(),
+		UID:                csv.GetUID(),
+		BlockOwnerDeletion: pointer.BoolPtr(false),
+		Controller:         pointer.BoolPtr(false),
+	}
+
+	meterDefinition.ObjectMeta.SetOwnerReferences([]metav1.OwnerReference{ref})
+	meterDefinition.ObjectMeta.Namespace = csv.Namespace
+
+	meterDefName := meterDefinition.Name
+	err = r.Client.Create(context.TODO(), &meterDefinition)
+	if err != nil {
+		reqLogger.Error(err, "Could not create meterdefinition", "mdef", meterDefName, "CSV", csv.Name)
+		return &ExecResult{
+			ReconcileResult: reconcile.Result{},
+			Err:             err,
+		}
+	}
+	reqLogger.Info("Created meterdefinition", "mdef", meterDefName, "CSV", csv.Name)
+
+	return &ExecResult{
+		ReconcileResult: reconcile.Result{Requeue: true},
+		Err:             nil,
+	}
+}
+
+//TODO: get this to work
 func deleteOnDiff(first *[]marketplacev1beta1.MeterDefinition, second *[]marketplacev1beta1.MeterDefinition) []marketplacev1beta1.MeterDefinition {
 	diff := []marketplacev1beta1.MeterDefinition{}
 	stringMapper := make(map[*marketplacev1beta1.MeterDefinition]bool)
@@ -360,7 +386,7 @@ func deleteOnDiff(first *[]marketplacev1beta1.MeterDefinition, second *[]marketp
 	return diff
 }
 
-func listAllCommunityMeterdefs(runtimeClient client.Client,indexLabels map[string]string)(*marketplacev1beta1.MeterDefinitionList,*ExecResult){
+func listAllCommunityMeterdefsOnCluster(runtimeClient client.Client,indexLabels map[string]string)(*marketplacev1beta1.MeterDefinitionList,*ExecResult){
 	installedMeterdefList := &marketplacev1beta1.MeterDefinitionList{}
 
 	// look for meterdefs that are from the meterdefinition catalog
@@ -436,104 +462,6 @@ func (r *DeploymentConfigReconciler) deleteAllMeterdefsForCsv(indexLabels map[st
 	return &ExecResult{
 		Status: ActionResultStatus(Continue),
 	}
-}
-
-func deleteMeterDefintions(namespace string, mdefNames []string, client client.Client, reqLogger logr.Logger) error {
-	for _, mdefName := range mdefNames {
-
-		installedMeterDefn := &marketplacev1beta1.MeterDefinition{}
-		err := client.Get(context.TODO(), types.NamespacedName{Name: mdefName, Namespace: namespace}, installedMeterDefn)
-		if err != nil && !errors.IsNotFound((err)) {
-			reqLogger.Error(err, "could not get meter definition", "Name", mdefName)
-			return err
-		}
-
-		// remove owner ref from meter definition before deleting
-		installedMeterDefn.ObjectMeta.OwnerReferences = []metav1.OwnerReference{}
-		err = client.Update(context.TODO(), installedMeterDefn)
-		if err != nil {
-			reqLogger.Error(err, "Failed updating owner reference on meter definition", "Name", mdefName, "Namespace", namespace)
-			return err
-		}
-		reqLogger.Info("Removed owner reference from meterdefintion", "Name", mdefName, "Namespace", namespace)
-
-		reqLogger.Info("Deleteing MeterDefinition")
-		err = client.Delete(context.TODO(), installedMeterDefn)
-		if err != nil && !errors.IsNotFound(err) {
-			reqLogger.Error(err, "could not delete MeterDefinition", "Name", mdefName)
-			return err
-		}
-		reqLogger.Info("Deleted meterdefintion", "Name", mdefName, "Namespace", namespace)
-	}
-	return nil
-}
-
-func createMeterDefintions(scheme *runtime.Scheme, client client.Client, namespace string, csvName string, mdefNames []string, meterDefsMap map[string]marketplacev1beta1.MeterDefinition, reqLogger logr.Logger) error {
-	// Fetch the ClusterServiceVersion instance
-	csv := &olmv1alpha1.ClusterServiceVersion{}
-	err := client.Get(context.TODO(), types.NamespacedName{Name: csvName, Namespace: namespace}, csv)
-	if err != nil {
-		reqLogger.Error(err, "could not fetch ClusterServiceversion isntance", "CSVName", csvName)
-		return err
-	}
-
-	gvk, err := apiutil.GVKForObject(csv, scheme)
-	if err != nil {
-		return err
-	}
-
-	// create owner reference instance
-	ref := metav1.OwnerReference{
-		APIVersion:         gvk.GroupVersion().String(),
-		Kind:               gvk.Kind,
-		Name:               csv.GetName(),
-		UID:                csv.GetUID(),
-		BlockOwnerDeletion: pointer.BoolPtr(false),
-		Controller:         pointer.BoolPtr(false),
-	}
-
-	// create meter definitions
-	for _, mdefName := range mdefNames {
-		meterDefn := meterDefsMap[mdefName]
-		meterDefn.ObjectMeta.SetOwnerReferences([]metav1.OwnerReference{ref})
-		meterDefn.ObjectMeta.Namespace = namespace
-
-		err = client.Create(context.TODO(), &meterDefn)
-		if err != nil {
-
-			reqLogger.Error(err, "Failed creating meter definition", "Name", mdefName, "Namespace", namespace)
-			return err
-		}
-
-		reqLogger.Info("Created meterdefintion", "Name", mdefName, "Namespace", namespace)
-	}
-	return nil
-}
-
-func updateMeterDefintions(namespace string, mdefNames []string, meterDefsMap map[string]marketplacev1beta1.MeterDefinition, client client.Client, reqLogger logr.Logger) error {
-	for _, mdefName := range mdefNames {
-		meterdefFromCluster := &marketplacev1beta1.MeterDefinition{}
-		err := client.Get(context.TODO(), types.NamespacedName{Name: mdefName, Namespace: namespace}, meterdefFromCluster)
-		if err != nil {
-			return err
-		}
-
-		//TODO: check what values we want to compare here
-		updatedMeterdefinition := meterdefFromCluster.DeepCopy()
-		updatedMeterdefinition.Spec = meterDefsMap[mdefName].Spec
-		updatedMeterdefinition.ObjectMeta.Annotations = meterDefsMap[mdefName].ObjectMeta.Annotations
-
-		if !reflect.DeepEqual(updatedMeterdefinition, meterdefFromCluster) {
-			reqLogger.Info("meterdefintion is out of sync with latest meterdef catalog", "Name", meterdefFromCluster.Name)
-			err = client.Update(context.TODO(), updatedMeterdefinition)
-			if err != nil {
-				reqLogger.Error(err, "Failed updating meter definition", "Name", mdefName, "Namespace", namespace)
-				return err
-			}
-			reqLogger.Info("Updated meterdefintion", "Name", mdefName, "Namespace", namespace)
-		}
-	}
-	return nil
 }
 
 func (r *DeploymentConfigReconciler) pruneDeployPods(latestVersion int64, request reconcile.Request, reqLogger logr.Logger) *ExecResult {
