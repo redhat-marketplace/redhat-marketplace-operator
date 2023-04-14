07e5dbb275efddfe3e42e3935f1dcf8de348e2d1
maxpaspa <maxpaspa@ibm.com>
2023-04-14 14:36:23 -0400

    test

--- .github/workflows/scripts/scan-rhm-images.sh
+++ .github/workflows/scripts/scan-rhm-images.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-IMAGE_REGISTRY="quay.io"
-IMAGE_REPO="rh-marketplace"
-IMAGE_TAG=$TAG
+IMAGE_REGISTRY_PATH=${IMAGE_REGISTRY}
+TEMP=(${IMAGE_REGISTRY_PATH//// })
+TARGET_REGISTRY="${TEMP[0]}"
+TARGET_REPO="${TEMP[1]}"
+TARGET_TAG="{$TAG}"
 
 declare -a IMAGE_NAMES=("redhat-marketplace-operator"
 	"redhat-marketplace-reporter"
@@ -10,7 +12,7 @@ declare -a IMAGE_NAMES=("redhat-marketplace-operator"
 	"redhat-marketplace-metric-state"
 	"ibm-metrics-operator"
 )
-
+echo ${IMAGE_REPO}
 SKOPEO_VERSION=$(skopeo --version)
 echo ${SKOPEO_VERSION}
 JQ_VERSION=$(jq --version)
@@ -23,11 +25,11 @@ for IMAGE_NAME in "${IMAGE_NAMES[@]}"; do
     echo "running $REPORT_NAME"
 
 	# Must get child digest
-	IMAGE_DIGEST=`skopeo inspect --raw docker://$IMAGE_REGISTRY/$IMAGE_REPO/$IMAGE_NAME:$IMAGE_TAG | jq -r '.manifests | .[] | select(.platform.architecture | contains("amd64")) | .digest'`
+	IMAGE_DIGEST=`skopeo inspect --raw docker://$TARGET_REGISTRY/$TARGET_REPO/$IMAGE_NAME:$IMAGE_TAG | jq -r '.manifests | .[] | select(.platform.architecture | contains("amd64")) | .digest'`
 	
 	echo "$IMAGE_NAME: $IMAGE_DIGEST"
 	
-	curl --retry 3 -s -o $REPORT_NAME https://$IMAGE_REGISTRY/api/v1/repository/$IMAGE_REPO/$IMAGE_NAME/manifest/$IMAGE_DIGEST/security?vulnerabilities=true
+	curl --retry 3 -s -o $REPORT_NAME https://$TARGET_REGISTRY/api/v1/repository/$TARGET_REPO/$IMAGE_NAME/manifest/$IMAGE_DIGEST/security?vulnerabilities=true
 	
 	# All Vulnerabilities
 	echo "=== $REPORT_NAME ALL VULNERABILITIES ==="
